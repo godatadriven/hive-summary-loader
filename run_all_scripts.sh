8bcef1f01c9f3b739f77d1ae4e375ed794d1a7de
Tunde <tunde.balint@gmail.com>
2014-03-31 15:20:34 +0200

    run all scripts from a directory

---
+++ run_all_scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pathDir=`pwd`/hive
+scripts_to_run_dir=$pathDir
+scripts_archive_dir=$pathDir/done
+mkdir -p $scripts_archive_dir
+for each in "$scripts_to_run_dir"/*.sh
+do 
+	bash $each
+	mv $each $scripts_archive_dir/
+done
